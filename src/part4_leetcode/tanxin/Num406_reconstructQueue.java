f095d14eb7d0aea4c1769e84aaadc7cfd6ca33d8
Your-Pluviophile <935664910@qq.com>
2022-12-14 20:13:26 +0800

    贪心一题

---
+++ src/part4_leetcode/tanxin/Num406_reconstructQueue.java
@@ -0,0 +1,20 @@
+package part4_leetcode.tanxin;
+
+import java.util.*;
+
+public class Num406_reconstructQueue {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (a, b) -> {
+            if (a[0] == b[0]) return a[1] - b[1];
+            return b[0] - a[0];
+        });
+
+
+        List<int[]> ans = new ArrayList<int[]>();
+        for (int[] person : people) {
+            ans.add(person[1], person);
+        }
+        return ans.toArray(new int[ans.size()][]);
+
+    }
+}

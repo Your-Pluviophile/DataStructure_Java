93dbc336ab45a918d8dd25d5daa8af2abb00cceb
Your-Pluviophile <935664910@qq.com>
2023-01-03 10:15:19 +0800

    思路想得到 coding不出来

--- src/part4_leetcode/array/Num704_search.java
+++ src/part4_leetcode/array/Num704_search.java
@@ -1,4 +1,25 @@
 package part4_leetcode.array;
 
 public class Num704_search {
+    public int search(int[] nums, int target) {
+        //剪枝
+        if(target < nums[0] || target > nums[nums.length - 1]){
+            return -1;
+        }
+        //下标
+        int left = 0;
+        int right = nums.length - 1;
+        //二分查找
+        while(left <= right){
+            int mid = left + ((right - left) >> 1);
+            if(target == nums[mid]){
+                return mid;
+            }else if(target > nums[mid]){
+                left = mid + 1;
+            }else if(target < nums[mid]){
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
 }

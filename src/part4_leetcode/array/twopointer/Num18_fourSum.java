d46f012fc2fea95614b8bb3803f06613a1da1953
Your-Pluviophile <935664910@qq.com>
2023-01-19 16:06:02 +0800

    左右指针

---
+++ src/part4_leetcode/array/twopointer/Num18_fourSum.java
@@ -0,0 +1,39 @@
+package part4_leetcode.array.twopointer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Num18_fourSum {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 4) return res;
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0 && nums[i] > target) return res;
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < nums.length; j++) {
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int left = j + 1;
+                int right = nums.length - 1;
+                while(left < right){
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target){
+                        res.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
+                        while(left < right && nums[left] == nums[left + 1]) left++;
+                        while(left < right && nums[right - 1] == nums[right]) right--;
+                        left++;
+                        right--;
+                    }else if (sum > target){
+                        right--;
+                    }else{
+                        left++;
+                    }
+                }
+
+            }
+        }
+        return res;
+    }
+}

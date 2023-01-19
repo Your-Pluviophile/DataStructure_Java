f6b7d7263751ab94ac96d1fa5c98a92990ea1852
Your-Pluviophile <935664910@qq.com>
2023-01-19 15:46:48 +0800

    左右指针

---
+++ src/part4_leetcode/array/twopointer/Num15_threeSum.java
@@ -0,0 +1,40 @@
+package part4_leetcode.array.twopointer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Num15_threeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int len = nums.length;
+        if (nums == null || len < 3) return res;
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < len; i++) {
+            if (nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // 去重
+            int target = -nums[i];
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) left++; // 去重
+                    while (left < right && nums[right] == nums[right - 1]) right--; // 去重
+                    left++;
+                    right--;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return res;
+    }
+}

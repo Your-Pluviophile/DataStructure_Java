a4913c93a134ae979d4edd040372e4f198cab960
Your-Pluviophile <935664910@qq.com>
2023-02-07 10:22:48 +0800

    二叉树先序遍历4

---
+++ src/part4_leetcode/bintree/traversal/Num572_isSubtree.java
@@ -0,0 +1,18 @@
+package part4_leetcode.bintree.traversal;
+
+public class Num572_isSubtree {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        //root到根节点
+        if(root == null) return false;
+        return isSameTree(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
